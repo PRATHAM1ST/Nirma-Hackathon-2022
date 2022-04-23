747608253dbe471d058c7918c1c79b7807c98bdb
PRATHAM1ST <the.listner01@gmail.com>
2022-04-23 14:02:37 +0530

    initial setup

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+export default function Home(){
+    return (
+        <container>
+            
+        </container>
+    )
+}
+
+// https://unsplash.com/photos/9Z1KRIfpBTM
+// https://source.unsplash.com/random/1920x1080/?wallpaper,landscape
