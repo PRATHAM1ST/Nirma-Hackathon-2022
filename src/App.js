6d6c52dbd2412b5e3faaece2b32cbcc6545a5388
PRATHAM1ST <the.listner01@gmail.com>
2022-04-24 13:32:58 +0530

    Create

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Home from "./pages/Home";
 import { Routes, Route, useParams } from "react-router-dom";
 import Signin from "./pages/Signin";
 import Register from "./pages/Register";
+import Page from "./pages/Page";
+import Create from "./pages/Create";
 
 // css
 import "./css/root.css";
@@ -10,7 +12,6 @@ import "./css/header.css";
 import "./css/program.css";
 import "./css/auth.css";
 import "./css/page.css";
-import Page from "./pages/Page";
 
 function App() {
   return (
@@ -20,7 +21,7 @@ function App() {
         <Route path="/SignIn" element={<Signin/>} />
         <Route path="/Register" element={<Register/>} />
         <Route path="/Page/:id" exact element={<Page/>} />
-        <Route path="/Create" exact element={<Page/>} />
+        <Route path="/Create" exact element={<Create/>} />
       </Routes>
     </div>
   );
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,18 +1,35 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "../elements/Header";
 
 export default function Register(){
+    const [title, setTitle] = useState('');
+    const [date, setDate] = useState('');
     return (
         <>
             <Header home={false} />
             <form className="form">
-                <input type="text" placeholder="Title" required/>
-                <input type="" placeholder="" required/>
-                <input type="text" placeholder="Paypal Id" required/>
-                <input type="text" placeholder="Username" required/>
-                <input type="password" placeholder="Password" required/>
-                <button>Register</button>
-                <Link to="/SignIn">Upload</Link>
+                <input 
+                    type="text" 
+                    placeholder="Title" 
+                    value={title} 
+                    onChange={(e)=> setTitle(e.target.value)} 
+                    required/>
+                <input 
+                    type="date" 
+                    placeholder="Deadline" 
+                    value={date} 
+                    onChange={(e)=> setTitle(e.target.value)} 
+                    required/>
+
+                <label>
+                    Eth: <input type="number" placeholder="Goal to achive" min={1} required/>
+                </label>
+                <textarea type="text" placeholder="Description" rows={6}></textarea>     
+                {/* <input type="file" multiple accept="image/jpg,image/png" id="fileToUpload" required/>  */}
+                <Link to="/"type="submit">
+                    <button>Upload</button>
+                </Link>
             </form>
         </>
     )
